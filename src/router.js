1a55d5cc7bb386996b929434e4c6f1a1928f21e1
hanerx <zhangyu199946@gmail.com>
2019-01-29 20:06:57 +0800

    搭了一个-500分的页面

--- src/router.js
+++ src/router.js
@@ -4,45 +4,46 @@ import Router from 'vue-router'
 Vue.use(Router);
 
 export default new Router({
-  routes: [
-    // 重定向到话题
-    { path: '/', name: 'index', redirect: {name: 'topic'} },
-    // 话题
-    { path: '/topic', name: 'topic', component: () => import('./views/Topic.vue') },
-    // 登录
-    { path: '/login', name: 'login', component: () => import('./views/Login.vue') },
-    // 设置
-    { path: '/settings', name: 'settings', component: () => import('./views/Settings.vue') },
-    // 用户个人页
-    {
-      path: '/account',
-      name: 'account',
-      redirect: {name: 'user-info'},
-      meta: {requireLogin: true},  // 需要登录
-      component: () => import('./views/Account.vue'),
-      children: [
-        // 用户信息
+    routes: [
+        // 重定向到话题
+        {path: '/', name: 'index', redirect: {name: 'topic'}},
+        // 话题
+        {path: '/topic', name: 'topic', component: () => import('./views/Topic.vue')},
+        // 登录
+        {path: '/login', name: 'login', component: () => import('./views/Login.vue')},
+        // 设置
+        {path: '/settings', name: 'settings', component: () => import('./views/Settings.vue')},
+        {path: '/school', name: 'school', component: () => import('./views/School.vue')},
+        // 用户个人页
         {
-          path: 'user-info',
-          name: 'user-info',
-          meta: {requireLogin: true},
-          component: () => import('./views/UserInfo.vue')
+            path: '/account',
+            name: 'account',
+            redirect: {name: 'user-info'},
+            meta: {requireLogin: true},  // 需要登录
+            component: () => import('./views/Account.vue'),
+            children: [
+                // 用户信息
+                {
+                    path: 'user-info',
+                    name: 'user-info',
+                    meta: {requireLogin: true},
+                    component: () => import('./views/UserInfo.vue')
+                },
+                // 积分详情
+                {
+                    path: 'exp-detail',
+                    name: 'exp-detail',
+                    meta: {requireLogin: true},
+                    component: () => import('./views/ExpDetail.vue')
+                },
+            ],
         },
-        // 积分详情
+        // 消息
         {
-          path: 'exp-detail',
-          name: 'exp-detail',
-          meta: {requireLogin: true},
-          component: () => import('./views/ExpDetail.vue')
+            path: '/message',
+            name: 'message',
+            meta: {requireLogin: true},  // 需要登录
+            component: () => import('./views/Message.vue')
         },
-      ],
-    },
-    // 消息
-    {
-      path: '/message',
-      name: 'message',
-      meta: {requireLogin: true},  // 需要登录
-      component: () => import('./views/Message.vue')
-    },
-  ],
+    ],
 });
