5336d597a3ee1f707a9b1d8c3176985802bd0d30
hanerx <zhangyu199946@gmail.com>
2019-01-29 20:06:57 +0800

    搭了一个-500分的页面
    自闭了

--- src/components/QuestionCard.vue
+++ src/components/QuestionCard.vue
@@ -1,36 +1,66 @@
 <template>
-  <div class="question-card">
-    <v-card id="card" flat border>
-      <v-container>
-        <v-layout row>
-          <v-flex pa-1>
-            <span class="font-weight-bold">{{ title }}</span>
-          </v-flex>
-          <v-flex v-if="img" shrink pa-1>
-            <img :src="img" width="150px" height="90px" alt="">
-          </v-flex>
-        </v-layout>
-        <v-layout row>
-          <v-flex class="font--text" xs12 pa-1>标签 : <span v-for="(tag, index) in tags" :key="index">{{ index===0 ? '' : '/' }}{{ tag }}</span></v-flex>
-        </v-layout>
-        <v-layout row>
-          <v-flex class="font--text" grow pa-1>{{ follow }} 人关注&nbsp;·&nbsp;{{ comment }} 条评论</v-flex>
-          <v-flex class="time--text" shrink pa-1>{{ edittime }}</v-flex>
-        </v-layout>
-      </v-container>
-    </v-card>
-  </div>
+    <div class="question-card">
+        <v-card id="card" flat border>
+            <!--<v-container v-if="card_type==0">-->
+            <!--<v-layout row>-->
+            <!--<v-flex pa-1>-->
+            <!--<span class="font-weight-bold">{{ title }}</span>-->
+            <!--<div>{{substring}}</div>-->
+            <!--</v-flex>-->
+            <!--<v-flex v-if="img" shrink pa-1>-->
+            <!--<img :src="img" width="150px" height="90px" alt="">-->
+            <!--</v-flex>-->
+            <!--</v-layout>-->
+            <!--<v-layout row>-->
+            <!--<v-flex class="font&#45;&#45;text" xs12 pa-1>标签 : <span v-for="(tag, index) in tags" :key="index">{{ index===0 ? '' : '/' }}{{ tag }}</span>-->
+            <!--</v-flex>-->
+            <!--</v-layout>-->
+            <!--<v-layout row>-->
+            <!--<v-flex class="font&#45;&#45;text" grow pa-1>{{ follow }} 人关注&nbsp;·&nbsp;{{ comment }} 条评论</v-flex>-->
+            <!--<v-flex class="time&#45;&#45;text" shrink pa-1>{{ edittime }}</v-flex>-->
+            <!--</v-layout>-->
+
+            <!--</v-container>-->
+            <v-card>
+
+                <v-img
+                        :src="img"
+                        aspect-ratio="2.75"
+                        v-if="img!=''"
+                ></v-img>
+
+                <v-card-title primary-title>
+                    <div>
+                        <h3>{{ title }}</h3>
+                        <div>{{substring}}
+                        </div>
+
+                    </div>
+
+                </v-card-title>
+
+                <v-card-actions>
+                    <v-flex class="font--text" xs12 pa-1>标签 : <span v-for="(tag, index) in tags" :key="index">{{ index===0 ? '' : '/' }}{{ tag }}</span>
+                    </v-flex>
+                    <v-flex class="font--text" grow pa-1>{{ follow }} 人关注&nbsp;·&nbsp;{{ comment }} 条评论 <span
+                            class="time--text">{{ edittime }}</span></v-flex>
+
+                </v-card-actions>
+            </v-card>
+        </v-card>
+    </div>
 </template>
 
 <script>
-  export default {
-    name: "QuestionCard",
-    props: ['title', 'img', 'tags', 'follow', 'comment', 'edittime'],
-  }
+    export default {
+        name: "QuestionCard",
+        props: ['title', 'img', 'tags', 'follow', 'comment', 'edittime', 'substring', 'card_type'],
+    }
 </script>
 
 <style scoped>
-  #card {
-    margin-top: 10px;
-  }
+    #card {
+        margin-top: 10px;
+    }
+
 </style>
