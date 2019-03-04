864ee4a436bd7a22b2cdb8ef7a9cdea0d31bf813
laipz8200 <laipz8200@icloud.com>
2019-03-04 16:50:48 +0800

    修改样式
    调整描述为None的卡片为不显示描述；

--- src/components/QuestionCard.vue
+++ src/components/QuestionCard.vue
@@ -33,7 +33,7 @@
                 <v-card-title primary-title>
                     <div>
                         <h3>{{ title }}</h3>
-                        <p class="description">{{description}}
+                        <p class="description">{{description !== 'None' ? description : ''}}
                         </p>
 
                     </div>
