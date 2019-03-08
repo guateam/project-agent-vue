212a6310b256fbacc81977998c704be9f793eaea
Kffhi <13979215061@163.com>
2019-03-08 20:54:37 +0800

    企业中心

--- src/views/CorporateCenter.vue
+++ src/views/CorporateCenter.vue
@@ -1,24 +1,198 @@
 <template>
-    <div>
+    <div class="bigbox">
         <div class="head">
             <div style="width: 45px;height: 45px;margin-left: 14px;display: flex;align-items: center;justify-content: center;">
-                <v-icon large color="white" @click="$router.push({name:'school'})">keyboard_arrow_left</v-icon>
+                <v-icon large color="white" @click="$router.push({name:'topic'})">keyboard_arrow_left</v-icon>
             </div>
             <span style="color: #fff;font-size: 20px;font-weight: 500;line-height: 1;letter-spacing: .02em;font-family: Roboto,sans-serif;">企业中心</span>
             <div style="width: 45px;height: 45px;margin-right: 14px;display: flex;align-items: center;justify-content: center;color: #fff;font-size: 17px;font-weight: 500;line-height: 1;letter-spacing: .02em;font-family: Roboto,sans-serif;">
             </div>
         </div>
         <div class="line"></div>
+        <Scroll :data="topimgs" style="z-index: 200">
+            <v-flex xs12>
+                <v-carousel hide-delimiters height="200px">
+                    <v-carousel-item
+                            v-for="(item,i) in topimgs"
+                            :key="i"
+                            :src="item.src"
+                    >
+                        <span class="banner">{{item.title}}</span>
+                    </v-carousel-item>
+                </v-carousel>
+            </v-flex>
+            <v-container grid-list-md text-xs-center>
+                <v-layout row wrap>
+                    <v-flex xs4 style="align-items: center;justify-content: space-between;flex-direction: column" @click="$router.push({name:'requirement-publish'})">
+                        <div>
+                            <v-icon large color="#99FF00">event_note</v-icon>
+                        </div>
+                        <div style="margin-top: 5px">发布需求</div>
+                    </v-flex>
+                    <v-flex xs4 style="align-items: center;justify-content: space-between;flex-direction: column" @click="$router.push({name:'requirement-check'})">
+                        <div>
+                            <v-icon large color="#99FFFF">library_books</v-icon>
+                        </div>
+                        <div style="margin-top: 5px">需求审核</div>
+                    </v-flex>
+                    <v-flex xs4 style="align-items: center;justify-content: space-between;flex-direction: column" @click="$router.push({name:'requirement-history'})">
+                        <div>
+                            <v-icon large color="#CC66CC">question_answer</v-icon>
+                        </div>
+                        <div style="margin-top: 5px">历史发布</div>
+                    </v-flex>
+                </v-layout>
+                <v-layout row wrap>
+                    <v-flex xs4 style="align-items: center;justify-content: space-between;flex-direction: column">
+                        <div>
+                            <v-icon large color="#FFFF33">access_time</v-icon>
+                        </div>
+                        <div style="margin-top: 5px">想不到了</div>
+                    </v-flex>
+                    <v-flex xs4 style="align-items: center;justify-content: space-between;flex-direction: column">
+                        <div>
+                            <v-icon large color="#6699FF">
+                                account_balance_wallet
+                            </v-icon>
+                        </div>
+                        <div style="margin-top: 5px">想不到了</div>
+                    </v-flex>
+                </v-layout>
+            </v-container>
+            <v-divider></v-divider>
+            <v-layout row wrap style="line-height: 1.5;height: 40px;align-items: center;justify-content: center;">
+                <v-flex xs2 style="text-align: center">
+                    <v-icon color="#FF0066">volume_up</v-icon>
+                </v-flex>
+                <v-flex xs9>
+                    <h3>你有一份新的重要通知待查看</h3>
+                </v-flex>
+                <v-flex xs1>
+                    <v-icon color="black">chevron_right</v-icon>
+                </v-flex>
+            </v-layout>
+            <v-divider></v-divider>
+            <v-layout row wrap style="line-height: 1.5;height: 40px;align-items: center;justify-content: center;">
+                <v-flex xs11>
+                    <h3>数据中心</h3>
+                </v-flex>
+                <v-flex xs1>
+                    <v-icon color="black">chevron_right</v-icon>
+                </v-flex>
+            </v-layout>
+            <v-container grid-list-md text-xs-center>
+                <v-layout row wrap>
+                    <v-flex xs4 style="align-items: center;flex-direction: column;border-right: 1px solid #eee">
+                        <div style="margin-top: 5px">粉丝</div>
+                        <div style="margin-top: 5px">24369</div>
+                        <div style="margin-top: 5px;color: #FF0033">
+                            <v-icon large color="#FF0033" small>
+                                keyboard_arrow_up
+                            </v-icon>
+                            366
+                        </div>
+                    </v-flex>
+                    <v-flex xs4 style="align-items: center;flex-direction: column;border-right: 1px solid #eee">
+                        <div style="margin-top: 5px">需求发布</div>
+                        <div style="margin-top: 5px">5465465</div>
+                        <div style="margin-top: 5px;color: #99FF00">
+                            <v-icon large color="#99FF00" small>
+                                keyboard_arrow_down
+                            </v-icon>
+                            366
+                        </div>
+                    </v-flex>
+                    <v-flex xs4 style="align-items: center;flex-direction: column">
+                        <div style="margin-top: 5px">需求浏览</div>
+                        <div style="margin-top: 5px">54655</div>
+                        <div style="margin-top: 5px;color: #99FF00">
+                            <v-icon large color="#99FF00" small>
+                                keyboard_arrow_down
+                            </v-icon>
+                            5566
+                        </div>
+                    </v-flex>
+                </v-layout>
+                <v-divider></v-divider>
+                <v-layout row wrap>
+                    <v-flex xs4 style="align-items: center;flex-direction: column;border-right: 1px solid #eee">
+                        <div style="margin-top: 5px">被关注数</div>
+                        <div style="margin-top: 5px">24369</div>
+                        <div style="margin-top: 5px;color: #FF0033">
+                            <v-icon large color="#FF0033" small>
+                                keyboard_arrow_up
+                            </v-icon>
+                            366
+                        </div>
+                    </v-flex>
+                    <v-flex xs4 style="align-items: center;flex-direction: column;border-right: 1px solid #eee">
+                        <div style="margin-top: 5px">达成交易</div>
+                        <div style="margin-top: 5px">5465465</div>
+                        <div style="margin-top: 5px;color: #99FF00">
+                            <v-icon large color="#99FF00" small>
+                                keyboard_arrow_down
+                            </v-icon>
+                            366
+                        </div>
+                    </v-flex>
+                    <v-flex xs4 style="align-items: center;flex-direction: column">
+                        <div style="margin-top: 5px">想不到了</div>
+                        <div style="margin-top: 5px">54655</div>
+                        <div style="margin-top: 5px;color: #99FF00">
+                            <v-icon large color="#99FF00" small>
+                                keyboard_arrow_down
+                            </v-icon>
+                            5566
+                        </div>
+                    </v-flex>
+                </v-layout>
+            </v-container>
+        </Scroll>
     </div>
 </template>
 
 <script>
+    import Scroll from '../components/Scroll'
+
     export default {
-        name: "corporate_center"
+        name: "corporate-center",
+        components: {
+            Scroll
+        },
+        data() {
+            return {
+                bottomNav: 0,
+                topimgs: [
+                    {
+                        src: 'https://www.asgardusk.com/images/portfolio/3.png',
+                        title: '创作新秀激励奖第二期启动'
+                    },
+                    {
+                        src: 'https://www.asgardusk.com/images/portfolio/4.png',
+                        title: '我的废柴发明'
+                    },
+                    {
+                        src: 'https://www.asgardusk.com/images/portfolio/6.png',
+                        title: '专+星研会'
+                    },
+                    {
+                        src: 'https://www.asgardusk.com/images/portfolio/7.png',
+                        title: '我心中的镇站之宝'
+                    }
+                ]
+            }
+        },
+        methods: {},
+        mounted() {
+        }
     }
 </script>
 
 <style scoped>
+
+    .bigbox {
+    }
+
     .head {
         z-index: 400;
         width: 100%;
@@ -36,5 +210,15 @@
         width: 100%;
         height: 4em;
         background-color: #eee;
+    }
+
+    .banner {
+        position: absolute;
+        bottom: 0;
+        width: 100%;
+        background: rgba(0, 0, 0, 0.5);
+        text-align: center;
+        font-size: 1.3em;
+        color: rgba(255, 255, 255, 0.9);
     }
 </style>
