b71bbe0782f08009a72c1077ffff5e69afa8a8ec
yun_cn <zzwyxl@163.com>
2019-03-19 23:10:43 +0800

    粉丝和关注页的标签页和active绑定

--- src/views/FanList.vue
+++ src/views/FanList.vue
@@ -16,7 +16,7 @@
 
         </v-toolbar>
         <v-card>
-            <v-tabs fixed-tabs color="primary">
+            <v-tabs fixed-tabs color="primary" v-model="active">
                 <v-tab
                         v-for="tab in tabs"
                         :key="tab.title"
@@ -139,6 +139,7 @@
         mounted() {
             this.get_my_follow();
             this.get_my_fans();
+            this.active = this.$route.query.active
             // this.active = this.$route.query.active;
         }
     }
